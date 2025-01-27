4dbfd3b491793f643e4222819a6516e1861fedb8
arjun-sir <arjunkhanna0108@gmail.com>
2025-01-27 17:01:11 +0530

    chore: updated schema

---
+++ prisma/migrations/20250127110136_add_relation/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - Added the required column `userId` to the `Gadget` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Gadget" ADD COLUMN     "DecommissionedAt" TIMESTAMP(3),
+ADD COLUMN     "userId" TEXT NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE "Gadget" ADD CONSTRAINT "Gadget_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
